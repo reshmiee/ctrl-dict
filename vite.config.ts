a7b036aa4e08686bd2414be532eae3c5a41ba010
Reshmi <reshmiyadav0570@gmail.com>
2026-06-22 12:56:10 +0530

    include admin.html in vite build

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/ctrl-dict/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin.html',
+      }
+    }
+  }
 })
